c825702c2346b834386055af69f428841487b0c0
WangYu <yu.wang@cn.yokogawa.com>
2021-10-20 19:19:50 +0800

    Modify project setting. Function can not work correctly, because it hasn't changed to position independent!

--- rtos/TEST_Dynamic_App/src/main.c
+++ rtos/TEST_Dynamic_App/src/main.c
@@ -1,11 +1,25 @@
 int var;
 
-void foo(void)
+void func1(void)
 {
-  var = 1;
+	var = 1;
 }
 
-void bar(void)
+void func2(void)
 {
-  var = 2;
+	var = 2;
+}
+
+void libEntry(void)
+{
+	register char *got asm ("r9");
+	char* gotBackup = got;
+	got = (char*)0x08080000;
+
+	var = 0;
+
+	func1();
+	func2();
+
+	got = gotBackup;
 }
